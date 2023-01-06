6b08737218c17d3cd845c8dc7565398bb72e83ff
abelzinhoecb <121394222+abelzinhoecb@users.noreply.github.com>
2023-01-06 13:53:15 -0300

    feat: controller my bet done and launching exceptions (#5)

--- src/ecbahia/controller/controller.py
+++ src/ecbahia/controller/controller.py
@@ -1,2 +1,22 @@
+from src.ecbahia.models.route.model import MyBet
+
+from src.core.checkers.checker import MyBetChecker as BetChk
+
+
 class MyBetValidator:
-    pass
+    @classmethod
+    def checker(cls, my_bet: MyBet) -> None:
+        if my_bet.descript is not None:
+            if not BetChk.is_string(word=my_bet.descript):
+                raise ValueError("error: descript must be an string.")
+
+        if not BetChk.is_str_greater(word=my_bet.descript):
+            raise ValueError("error: descript must be greater than 0.")
+
+        elif BetChk.time_equals(time=my_bet.time):
+            raise ValueError("error: time must be greater than 0.")
+
+        elif not BetChk.money_greater(money=my_bet.money):
+            raise ValueError("error: money must be greater than 0.")
+        elif not BetChk.money_greater(money=my_bet.fee):
+            raise ValueError("error: fee must be greater than 0.")
