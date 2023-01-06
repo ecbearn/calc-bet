bd7edeca81fea32760c6f35a905176e624776fe9
abelzinhoecb <abelzinho.ecbahia@gmail.com>
2023-01-06 20:47:43 -0300

    feat: controller checker to multi bet

--- src/ecbahia/controller/controller.py
+++ src/ecbahia/controller/controller.py
@@ -1,4 +1,6 @@
-from src.ecbahia.models.route.model import MyBet
+from decimal import Decimal
+
+from src.ecbahia.models.route.model import MyBet, MyBetMulti
 
 from src.core.checkers.checker import MyBetChecker as BetChk
 
@@ -20,3 +22,17 @@ class MyBetValidator:
             raise ValueError("error: money must be greater than 0.")
         elif not BetChk.money_greater(money=my_bet.fee):
             raise ValueError("error: fee must be greater than 0.")
+
+    @classmethod
+    def checker_bet_multi(cls, my_bets: MyBetMulti) -> None:
+        if BetChk.money_smaller(money=my_bets.money, cash=my_bets.min_money):
+            raise ValueError("error: your bet doesn't match the minimal value.")
+
+        elif len(my_bets.my_bets) == 0:
+            raise ValueError("error: you don't have any odd to calculate.")
+
+        checker = map(lambda e: e.fee > Decimal("0.99") and e.is_multi, my_bets.my_bets)
+        checker = tuple(c for c in checker)
+
+        if len(checker) < 1:
+            raise ValueError("error: it was found a invalid bet odd.")
--- src/ecbahia/models/route/model.py
+++ src/ecbahia/models/route/model.py
@@ -1,13 +1,19 @@
 from decimal import Decimal
 
-from typing import Optional
+from typing import Optional, List
 
 from pydantic import BaseModel
 
 
 class MyBet(BaseModel):
-    money: Decimal
+    money: Decimal = Decimal("0.00")
     descript: Optional[str] = None
     time: Optional[int] = 1
     fee: Optional[Decimal] = Decimal("0.20")
     is_multi: Optional[bool] = False
+
+
+class MyBetMulti(BaseModel):
+    money: Decimal = Decimal("0.00")
+    my_bets: List[MyBet] = []
+    min_money: Decimal = Decimal("0.50")
