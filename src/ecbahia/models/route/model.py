1547e44dc54f1bbb4a1eceb2f7f795348313ea49
abelzinhoecb <abelzinho.ecbahia@gmail.com>
2023-01-06 13:30:46 -0300

    feat: created a checker and refactor codes need

--- src/ecbahia/models/route/model.py
+++ src/ecbahia/models/route/model.py
@@ -7,4 +7,6 @@ from pydantic import BaseModel
 
 class MyBet(BaseModel):
     money: Decimal
+    descript: Optional[str] = None
+    time: Optional[int] = 1
     fee: Optional[Decimal] = Decimal("0.20")
