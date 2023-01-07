dc55e30f945eeb324447d9053ae56d671de302a5
abelzinhoecb <abelzinho.ecbahia@gmail.com>
2023-01-06 21:14:02 -0300

    feat: calculate multi bet result

---
+++ src/core/utils/multiply.py
@@ -0,0 +1,18 @@
+from decimal import Decimal
+from typing import List
+
+from src.core.money.money import get_money
+from src.ecbahia.models.route.model import MyBet
+
+
+def multiply_list(my_odds: List[MyBet]) -> Decimal:
+    all_ods = [bet.fee for bet in my_odds]
+
+    multi_odds = Decimal("1.00")
+
+    for odd in all_ods:
+        multi_odds *= odd
+    else:
+        multi_odds = get_money(money=multi_odds)
+
+    return multi_odds
--- src/ecbahia/calc_bet/calc_bet.py
+++ src/ecbahia/calc_bet/calc_bet.py
@@ -1,7 +1,10 @@
 from src.core.money.money import get_money
 
+from src.core.utils.multiply import multiply_list
+
 from src.ecbahia.models.data.model import MyBet as BetResponse
 from src.ecbahia.models.route.model import MyBet as BetRequest
+from src.ecbahia.models.route.model import MyBetMulti
 
 
 def profit(my_bet: BetRequest) -> BetResponse:
@@ -31,3 +34,24 @@ def amount(my_bet: BetRequest) -> BetResponse:
     my_profit.amount = my_amount
 
     return my_profit
+
+
+def multi_bet(my_bets: MyBetMulti) -> BetResponse:
+    multi_ods = multiply_list(my_odds=my_bets.my_bets)
+
+    bet_response = BetResponse()
+    bet_response.is_multi = True
+    bet_response.descript = "This is a Multiply Bet!"
+    bet_response.fee = multi_ods
+    bet_response.capital = my_bets.money
+
+    total_amount = bet_response.fee * bet_response.capital
+    total_amount = get_money(total_amount)
+
+    total_profit = total_amount - bet_response.capital
+    total_profit = get_money(total_profit)
+
+    bet_response.amount = total_amount
+    bet_response.profit = total_profit
+
+    return bet_response
